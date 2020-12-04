0a58420168c90dca15fb45cf10457b8ff531d05d
Alekseenkov <69481564+Alekseenkov@users.noreply.github.com>
2020-12-04 15:32:28 +0300

    Add files via upload

---
+++ Leonid9.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include"FloppyDisk.h"
+
+int main()
+{
+	FloppyDisk FloppyDisk;
+
+	FloppyDisk.comand();
+	FloppyDisk.comand();
+	FloppyDisk.comand();
+	FloppyDisk.print_list();
+	FloppyDisk.comand();
+	FloppyDisk.print_list();
+
+
+
+}
+
